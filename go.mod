f83bc9a136afb6f4fc86f66d64d8f67b2e18753a
patrick mvula <mvlapatrick@gmail.com>
2024-08-13 04:21:48 +0200

    config setup

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
-module goshopapi
+module github.com/patrickmvla/ecomapi
 
 go 1.22.6
+
+require (
+	github.com/caarlos0/env v3.5.0+incompatible // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
---
+++ pkg/config/config.go
@@ -0,0 +1,59 @@
+package config
+
+import (
+	"log"
+	"path/filepath"
+	"time"
+	"runtime"
+
+	"github.com/joho/godotenv"
+	"github.com/caarlos0/env"
+)
+
+
+const (
+	ProductionEnv = "production"
+
+	DatabaseTimeout = 5 * time.Second
+	ProductionCachingTime = 1 * time.Second
+)
+
+var AuthIgnoreMethods = []string{
+	"user.UserService/Login",
+	"user.UserService/Register",
+}
+
+type Schema struct {
+	Environment		string `env:"environment"`
+	HttpPort		int		`env:"http_port"`
+	GrpcPort		int		`env:"grpc_port"`
+	AuthSecret		string	`env:"auth_secret"`
+	DatabaseURI		string	`env:"database_uri"`
+	RedisURI		string	`env:"redis_uri"`
+	RedisPassword	string	`env:"redis_password"`
+	RedisDB			string	`env:"redis_db"`
+}
+
+var (
+	cfg Schema
+)
+
+func LoadConfig() *Schema {
+	_, filename, _, _ := runtime.Caller(0)
+	currentDir := filepath.Dir(filename)
+
+	err := godotenv.Load(filepath.Join(currentDir, "config.yaml"))
+	if err != nil {
+		log.Printf("Error on loading configuration file, error: %v", err)
+	}
+
+	if err := env.Parse(&cfg); err != nil {
+		log.Fatalf("Error on parsing configuration file, error: %v", err)
+	}
+
+	return &cfg
+}
+
+func GetConfig() *Schema {
+	return &cfg
+}
